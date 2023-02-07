a1d9d9c4fa5512609ae6bb4fa049ef0e9f44cb2b
IbacacheSalgado <se.salgado@duocuc.cl>
2023-02-06 21:33:34 -0300

    modificamos

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+const { src, dest } = require("gulp-sass")(require('sass'));
+const sass = require("sass");
 
-function tarea( done ) {
-    console.log('Mi primera tarea');
+function css( done ) {
+    src('src/scss/app.scss')// Identificar el archivo de SASS
+        .pipe(sass()) // Compilarlo
+        .pipe( dest(bulid/css));// Almacenarlo en el disco
 
-    done();
+    done();// Callback que avisa a gulp cuando llegamos al final
 }
-
-exports.tarea = tarea;
+exports.css = css;
