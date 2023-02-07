58e077817c0e423ae908ce41919a216a79fccb1d
IbacacheSalgado <se.salgado@duocuc.cl>
2023-02-07 12:29:46 -0300

    agrego plumber para que no se caiga el proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const plumber = require('gulp-plumber');
 function css( done ) {
     src('src/scss/**/*.scss')// Identificar el archivo de SASS
-        .pipe(sass()) // Compilarlo
+        .pipe( plumber())
+        .pipe( sass()) // Compilarlo
         .pipe( dest('build/css'))// Almacenarlo en el disco
 
     done();// Callback que avisa a gulp cuando llegamos al final
