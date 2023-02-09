e655bf30a1f9396994be1ef549f9b2107aa2139e
IbacacheSalgado <se.salgado@duocuc.cl>
2023-02-09 15:53:47 -0300

    agregamos todas la imagens a js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+// Creamos galeria con javaScript
+// cuando tenemos muchas imagenes podemos crear una galeria de esta forma
 document.addEventListener('DOMContentLoaded', function(){
     iniciarApp();
 });
@@ -9,5 +11,13 @@ function iniciarApp() {
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes');
 
-    galeria.textContent = 'Vamos a crear la galeria';
+    for(let i = 1; i <= 12; i++) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumbs/${i}.avif" type="image/avif">
+            <source srcset="build/img/thumbs/${i}.webp" type="image/webp">
+            <img loading="lazy"  src="build/img/thumbs/${i}.jpg" alt="imagen galeria"></img>`;
+        
+        galeria.appendChild(imagen);
+    }
 }
