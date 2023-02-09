e3e1c8f07736f220ce48310b312eb9f79fd660e0
IbacacheSalgado <se.salgado@duocuc.cl>
2023-02-09 15:28:25 -0300

    ageragamos function a gulp

--- gulpfile.js
+++ gulpfile.js
@@ -49,13 +49,20 @@ function versionAvif( done ) {
         .pipe( dest( 'build/img' ));// guarda las imagenes en carpeta build
     done();
 }
+function javascript( done ) {
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'));
+    done();
+}
 
 function dev( done ) {
     watch('src/scss/**/*.scss', css);
+    src('src/js/**/*.js', javascript)
     done();
 }
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel ( imagenes, versionWebp, versionAvif, dev );
+exports.dev = parallel ( imagenes, versionWebp, versionAvif, javascript, dev );
