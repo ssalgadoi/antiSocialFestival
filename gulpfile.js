634c380c093e256241ac3938df9eeb4926bbc5a7
IbacacheSalgado <se.salgado@duocuc.cl>
2023-02-10 21:48:41 -0300

    ultimo arreglos

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,16 @@ const { src, dest, watch, parallel } = require('gulp');
 // CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps')
+
+
+// Javascript
+const terser = require('gulp-terser-js');
+
+
 
 // Imagenes
 // nueva funcion
@@ -14,8 +24,11 @@ const avif = require('gulp-avif');
 
 function css( done ) {
     src('src/scss/**/*.scss')// Identificar el archivo de SASS
+        .pipe( sourcemaps.init())
         .pipe( plumber())
         .pipe( sass()) // Compilarlo
+        .pipe( postcss([autoprefixer(), cssnano()]))
+        .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css'))// Almacenarlo en el disco
 
     done();// Callback que avisa a gulp cuando llegamos al final
@@ -51,7 +64,11 @@ function versionAvif( done ) {
 }
 function javascript( done ) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
+
     done();
 }
 
